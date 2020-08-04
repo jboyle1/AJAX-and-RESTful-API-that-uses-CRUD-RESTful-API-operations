056dec9866577abc882faa70863c1bf5753d4ae1
Jed Boyle <jedboyle01@gmail.com>
2020-08-04 21:18:43 +0100

    Create POST 'Create' request.

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
 const express = require('express');
-// Place express module in a variable called 'app'.
 const app = express();
-// Require path for static file use.
+const bodyParser = require('body-parser');
 
 let products = [
-    {
+{
     id: 1,
     name: 'laptop'
-    }, {
+},
+{
     id: 2,
-    name:'microwave'
-    }
+    name: 'microwave'
+}
 ];
 
 let currentId = 2;
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
 
 app.get('/products', function(req, res) {
     res.send({ products: products });
-})
+});
+
+app.post('/products', function(req, res) {
+    let productName = req.body.name;
+    currentId++;
+
+    products.push({
+        id: currentId,
+        name: productName
+    });
+
+    res.send('Successfully created product!');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
