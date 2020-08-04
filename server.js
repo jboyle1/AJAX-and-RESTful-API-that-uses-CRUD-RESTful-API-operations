c862282a9edf5bbdb38890e582ccda242fb14cd7
Jed Boyle <jedboyle01@gmail.com>
2020-08-04 20:37:59 +0100

    Create index.html and create form and submit button. Add configuration to server.js including port listener and product object.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// Place express module in a variable called 'app'.
+const app = express();
+// Require path for static file use.
+
+let products = [
+    {
+    id: 1,
+    name: 'laptop'
+    }, {
+    id: 2,
+    name:'microwave'
+    }
+];
+
+let currentId = 2;
+
+app.use(express.static(__dirname));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
