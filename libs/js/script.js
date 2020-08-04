d1a5c6959637a95e994983ef643162a8205a6875
Jed Boyle <jedboyle01@gmail.com>
2020-08-04 21:04:03 +0100

    Complete REAE/GET command

--- libs/js/script.js
+++ libs/js/script.js
@@ -5,7 +5,22 @@ $(function() {
             url: '/products',
             contentType: 'application/json',
             success: function(response) {
-                console.log(response);
+                var tbodyEl = $('tbody');
+
+                tbodyEl.html('');
+
+                response.products.forEach(function(product) {
+                    tbodyEl.append('\
+                        <tr>\
+                            <td class="id">' + product.id + '</td>\
+                            <td><input type="text" class="name" value=""' + product.name + '"></td>\
+                            <td>\
+                                <button class="update-button">UPDATE/PUT</button>\
+                                <button class="delete-button">DELETE</button>\
+                            </td>\
+                        </tr>\
+                    ');
+                });
             }
         });
     });
